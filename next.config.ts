6d614b575101dd4c9545602cc10f0032f64e4d92
Yacoubbeltanivelle <yacoubbeltanivelle@gmail.com>
2026-02-11 15:58:49 +0100

    chore: update config for repo name change

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: isProd ? "export" : undefined,
-  basePath: isProd ? "/PEP-Formation" : "",
-  assetPrefix: isProd ? "/PEP-Formation/" : "",
+  basePath: isProd ? "/Pep-formations-prototype-v1" : "",
+  assetPrefix: isProd ? "/Pep-formations-prototype-v1/" : "",
   images: {
     unoptimized: true,
   },
